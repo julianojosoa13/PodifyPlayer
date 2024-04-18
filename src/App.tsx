7b245fd28e4a9bc5f2cab7d520f82bafa6dfad93
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2024-04-18 15:59:20 +0300

    Setup of Path aliases

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import SignUp from '@views/auth/SignUp';
 
 const App = () => {
-  return <SafeAreaView></SafeAreaView>;
+  return (
+    <SafeAreaView>
+      <SignUp />
+    </SafeAreaView>
+  );
 };
 
 export default App;

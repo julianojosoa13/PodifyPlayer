96638e874a5726b5f80551bfbc5ba65b0864bc14
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2024-05-06 23:58:06 +0300

    Redux toolkit setup and AuthSlice

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import {StyleSheet} from 'react-native';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AuthNavigator from './navigation/auth/AuthNavigator';
+import {Provider} from 'react-redux';
+import store from './store';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <AuthNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <AuthNavigator />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
